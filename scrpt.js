995c398f2e7d51607baa5907ef02b61d653af2f8
krone50 <156448702+krone50@users.noreply.github.com>
2025-05-29 19:55:55 +0200

    Update scrpt.js

--- scrpt.js
+++ scrpt.js
@@ -4,18 +4,6 @@ const navLinks = document.querySelector(".nav-links");
 mobileMenu.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
-window.addEventListener('scroll', function() {
-        const footer = document.querySelector('footer');
-        const scrollPosition = window.scrollY + window.innerHeight; // Aktuelle Scroll-Position + Höhe des Fensters
-        const documentHeight = document.documentElement.scrollHeight; // Gesamthöhe des Dokuments
-
-        // Überprüfen, ob der Benutzer am Ende der Seite ist
-        if (scrollPosition >= documentHeight) {
-            footer.classList.add('visible'); // Fügt die "visible"-Klasse hinzu, um den Footer anzuzeigen
-        } else {
-            footer.classList.remove('visible'); // Entfernt die "visible"-Klasse, um den Footer zu verstecken
-        }
-    });
 
 document.addEventListener('DOMContentLoaded', function () {
   const toggle = document.getElementById('darkmode-toggle');
